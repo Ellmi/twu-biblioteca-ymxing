1b1180c0b354159e8d2a79584c628d8fd4da6cba
Yanmin <yanmin.xing@rea-group.com>
2015-02-22 00:33:54 +0800

    [Biblioteca-Release1] Yanmin:use TDD to add the test and the feature show welcome message when application start.

---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,34 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaAppTest {
+
+    private BibliotecaApp bibliotecaApp;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        bibliotecaApp = new BibliotecaApp();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void should_return_welcome_message_when_star_app(){
+        bibliotecaApp.showWelcomeWords();
+        assertEquals("Welcome use Biblioteca!\n", outContent.toString());
+    }
+
+}
