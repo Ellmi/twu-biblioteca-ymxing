66ba4f9763b78e9b40f1b52b0ed877055183a5c2
Yanmin <yanmin.xing@rea-group.com>
2015-02-22 09:11:47 +0800

    [Biblioteca-Release1] Yanmin:add the test for feature list books.

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -13,11 +13,18 @@ public class BibliotecaAppTest {
 
     private BibliotecaApp bibliotecaApp;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private String[] libraryBooks;
 
     @Before
     public void setUp() throws Exception {
         bibliotecaApp = new BibliotecaApp();
         System.setOut(new PrintStream(outContent));
+        libraryBooks = new String[5];
+        libraryBooks[0] = "The Jungle Book";
+        libraryBooks[1] = "The Boy in the Striped Pyjamas";
+        libraryBooks[2] = "Black Beauty";
+        libraryBooks[3] = "The Secret Garden";
+        libraryBooks[4] = "Peter Pan";
     }
 
     @After
@@ -29,6 +36,16 @@ public class BibliotecaAppTest {
     public void should_show_welcome_message_when_star_app(){
         bibliotecaApp.showWelcomeWords();
         assertEquals("Welcome use Biblioteca!\n", outContent.toString());
+    }
+
+    @Test
+    public void should_show_library_books_list(){
+        String libraryBooksString = "";
+        bibliotecaApp.showLibraryBooks();
+        for(int i=0;i<libraryBooks.length;i++){
+            libraryBooksString +=libraryBooks[i]+"\n";
+        }
+        assertEquals(libraryBooksString,outContent.toString());
     }
 
 }
